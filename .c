2cda7a65dee7dbeb04f3dca3e27401e0d7073bca
andre-kiyuna <dehljkdl@gmail.com>
2025-10-22 20:38:36 -0300

    Update .c

--- .c
+++ .c
@@ -1,37 +1,47 @@
 #include <stdio.h>
 
-void bemvindo(){
-    printf ("O objetivo do programa e calcular a media de consumo de agua por ano\n");
-}
-void atelogo(){
-    printf ("fim do programa");
-}
+void bemvindo();
+void atelogo();
+float registrarConsumo(float agua[], float media);
+float calcularMedia(float agua[], float media);
 int main()
 {
     bemvindo();
-    float agua[13];
+    float agua[13], media;
     agua[0]=0;
     int i;
-    for(i=1; i<13; i++){
-    printf ("Digite o consumo de agua do %d°mes: ", i);
-    scanf ("%f", &agua[i]);
-    agua[0] = agua[i] + agua[-i];
-    }
-    agua[0] / 12;
-    printf ("A media e: %f\n", agua[0]);
-    for(i=0; i<13; i++){
-    printf("%d°mes e: %f\n", i, agua[i]);
-    }
-    registrarConsumo();
+    registrarConsumo(agua, media);
+    calcularMedia(agua, media);
     atelogo();
     return 0;
 }
-float registrarConsumo(){
+void bemvindo(){
+    printf ("O objetivo do programa e calcular a media de consumo de agua por ano\n");
+}
+void atelogo()
+{
+    printf ("fim do programa");
+}
+float registrarConsumo(float agua[], float media){
     float consumo=0;
+    media = 0;
     int i;
+    for(i=1; i<13; i++){
+        printf ("Digite o consumo de agua do %d°mes: ", i);
+        scanf ("%f", &agua[i]);
+    media = agua[i] + agua[-i];
+    }
+    for(i=1; i<13; i++){
+        printf("%d°mes e: %f\n", i, agua[i]);
+    }
     for(i=1; i<13; i++){
     consumo = consumo + agua[i];
     }
-    printf ("O consumo do ano: %f", consumo);
+    printf ("O consumo do ano: %f\n", consumo);
     return consumo;
+}
+float calcularMedia(float agua[13], float media){
+    agua[0] = media / 12;
+    printf ("A media e: %f\n", agua[0]);
+    return agua, media;
 }
